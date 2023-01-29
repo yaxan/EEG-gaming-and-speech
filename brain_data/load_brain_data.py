c7038024786250aeffac902098e98a2c6757152c
yazan <ymasoud@uwaterloo.ca>
2023-01-29 00:04:48 -0500

    initial commit

---
+++ brain_data/load_brain_data.py
@@ -0,0 +1,138 @@
+"""
+PROGRAM DESCRIPTION:
+
+Loads saved brain data into two lists (relaxed and concentrated) which contain time series data in numpy array format. The program
+then assumes the relaxed and concentrated RMS values follow a gaussian distribution. The best cutoff RMS voltage to distinguish 
+between relaxed and concentrated states is then the intersection of these gaussians, and the probability of incorrect
+classification of the mental state is equal to the overlap area of gaussians. All this info is summarized in a plot of RMS
+histogram, the infered gaussian distributions, and a vertical line at cutoff RMS voltage.
+
+This program also plots the first sample of brain data's raw data, power spectrum, and brain wave (which is raw data limited to
+8Hz-12Hz frequencies)
+"""
+
+import os
+import sys
+sys.path.insert(1, os.path.dirname(os.getcwd())) #This allows importing files from parent folder
+import numpy as np
+from scipy.stats import norm
+import matplotlib.pyplot as plt
+import pickle
+from analysis_tools import get_power_spectrum, get_rms_voltage, gaussian_eval, get_brain_wave
+
+#These values should match saved data being loaded
+ACQTIME = 5
+SPS = 920 #Samples per second to collect data. Options: 128, 250, 490, 920, 1600, 2400, 3300.
+nsamples = int(ACQTIME*SPS)
+sinterval = 1.0/SPS
+freq_min = 8 #min freq of alpha waves
+freq_max = 12 #max freq of alpha waves
+freq = np.fft.fftfreq(nsamples, d=1.0/SPS)
+
+"""
+Asks user where to locate data
+"""
+while True:
+    person_name = input('Please enter name to load data (hak/ryan/ruining):')
+    if person_name in ['hak', 'ryan', 'ruining']:
+        break
+    else:
+        print('Please enter a correct formatted name')
+while True:        
+    save_folder = input('Please input folder name to load:')
+    save_path = os.path.join(person_name, save_folder)
+    if os.path.isdir(save_path): #Check if already a directory
+        break
+    else:
+        print("Please enter an existing folder name")
+
+"""
+Loads data files
+"""
+relaxed_file = os.path.join(save_path,'relaxed.pickle')
+concentrated_file = os.path.join(save_path,'concentrated.pickle')
+
+file = open(relaxed_file, 'rb')
+relaxed_data = pickle.load(file) #shape: [num relaxed data samples, length of time series]
+file.close()
+file = open(concentrated_file, 'rb')
+concentrated_data = pickle.load(file)
+file.close()
+print("There are", len(relaxed_data), "relaxed data samples")
+print("There are", len(concentrated_data), "concentrated data samples")
+relaxed_data = np.array(relaxed_data) 
+concentrated_data = np.array(concentrated_data)
+
+"""
+Calculates statistics on data
+"""
+freq = np.fft.fftfreq(nsamples, d=1.0/SPS) #frequencies for FFT of data
+relaxed_rms = np.zeros(len(relaxed_data))
+concentrated_rms = np.zeros(len(concentrated_data))
+
+#Gets RMS values of loaded brain data
+for index, time_series in enumerate(relaxed_data):
+    ps = get_power_spectrum(time_series)
+    relaxed_rms[index] = get_rms_voltage(ps, freq_min, freq_max, freq, nsamples)
+for index, time_series in enumerate(concentrated_data):
+    ps = get_power_spectrum(time_series)
+    concentrated_rms[index] = get_rms_voltage(ps, freq_min, freq_max, freq, nsamples)
+
+V0, wrong_relax, wrong_concentrate = gaussian_eval(relaxed_rms, concentrated_rms)
+#These values are needed for plotting gaussian distributions
+r_mean = np.mean(relaxed_rms)
+r_std = np.std(relaxed_rms)
+c_mean = np.mean(concentrated_rms)
+c_std = np.std(concentrated_rms)
+xpoints = np.linspace(c_mean-4*c_std, r_mean+4*r_std, 1000)
+
+"""
+Plots results of gaussian analysis
+"""
+fig, ax = plt.subplots()
+ax.hist(relaxed_rms, bins=6, density=True, label='Relaxed')
+ax.hist(concentrated_rms, bins=6, density=True, label='Concentrated')
+ax.plot(xpoints, norm.pdf(xpoints, r_mean, r_std), linestyle='--', color='red') #Relaxed gaussian dist
+ax.plot(xpoints, norm.pdf(xpoints, c_mean, c_std), linestyle='--', color='green') #Concentrated gaussian dist
+plt.axvline(V0, color='purple', linestyle='--', label='Cutoff Voltage')
+ax.set(xlabel='RMS Alpha Wave Voltage (V)', ylabel='Frequency', title='Concentrated / Relaxed EEG Data')
+ax.legend()
+
+"""
+Calculates power spectrum and true brain wave of the first relaxed and concentrated state then plots them all
+"""
+view_relaxed = relaxed_data[0]
+ps_relaxed = get_power_spectrum(view_relaxed)
+view_concentrated = concentrated_data[0]
+ps_concentrated = get_power_spectrum(view_concentrated)
+brain_relaxed = get_brain_wave(view_relaxed, 8, 12, freq)
+brain_concentrated = get_brain_wave(view_concentrated, 8, 12, freq)
+times = np.arange(0, ACQTIME, sinterval)
+
+fig2, ax2 = plt.subplots()
+ax2.plot(times, view_relaxed)
+ax2.set(xlabel='Time (s)', ylabel='Voltage (V)', title='Relaxed State Raw Data')
+fig3, ax3 = plt.subplots()
+ax3.plot(times, view_concentrated)
+ax3.set(xlabel='Time (s)', ylabel='Voltage (V)', title='Concentrated State Raw Data')
+
+fig3, ax3 = plt.subplots()
+ax3.plot(freq[0:250], ps_relaxed[0:250])
+ax3.set(xlabel='Frequency (Hz)', ylabel='Power', title='Relaxed State Power Spectrum')
+fig4, ax4 = plt.subplots()
+ax4.plot(freq[0:250], ps_concentrated[0:250])
+ax4.set(xlabel='Frequency (Hz)', ylabel='Power', title='Concentrated State Power Spectrum')
+
+fig3, ax3 = plt.subplots()
+ax3.plot(times, brain_relaxed)
+ax3.set(xlabel='Time (s)', ylabel='Voltage (V)', title='Relaxed State Brain Wave')
+fig4, ax4 = plt.subplots()
+ax4.plot(times, brain_concentrated)
+ax4.set(xlabel='Time (s)', ylabel='Voltage (V)', title='Concentrated State Brain Wave')
+plt.show()
+
+print('Best cutoff RMS voltage is', V0)
+print('Chance of getting incorrect classification is', 50*(wrong_relax+wrong_concentrate))
+input('Press <Enter> to end program')
+plt.close('all')
+
---
+++ brain_data/save_brain_data.py
@@ -0,0 +1,141 @@
+"""
+Program Description:
+
+We use this data to collect brain wave data for the concentrating and relaxed state and save it with
+pickle. Each person gets their own folder, inside this there are separate session folders, inside each of these there is a relaxed.pickle
+and concentrated.pickle file which contain a list of the different numpy array time series.
+"""
+import os
+import sys
+sys.path.insert(1, os.path.dirname(os.getcwd())) #This allows importing files from parent folder
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+from analysis_tools import get_power_spectrum, get_rms_voltage, get_brain_wave
+from Adafruit import ADS1x15
+
+ACQTIME = 5
+SPS = 920 #Samples per second to collect data. Options: 128, 250, 490, 920, 1600, 2400, 3300.
+VRANGE = 6144 #Full range scale in mV. Options: 256, 512, 1024, 2048, 4096, 6144.
+nsamples = int(ACQTIME*SPS)
+sinterval = 1.0/SPS
+freq_min = 8 #min freq of alpha waves
+freq_max = 12 #max freq of alpha waves
+
+print()
+print('Initializing ADC...')
+print()
+
+adc = ADS1x15()
+
+"""
+Ask user to specify what folder to save in
+"""
+while True:
+    person_name = input('Please enter your name (hak/ryan/ruining):')
+    if person_name in ['hak', 'ryan', 'ruining']:
+        break
+    else:
+        print('Please enter a correct formatted name')
+        
+save_folder = input('Please input folder name to make or save to:')
+save_path = os.path.join(person_name, save_folder)
+if not os.path.isdir(save_path): #Check if already a directory
+    try:
+        os.mkdir(save_path)
+        print("Successfully created folders")
+    except OSError:
+        print("Creation of the folder %s failed" % save_path)
+else:
+    print("Successfully connected to the folder %s " % save_path)
+    
+while True:
+    wavetype = input('Record relaxed or concentrated waves? Type (r/c)')
+    if wavetype in ['r','c']:
+        break   
+    else:
+        print('Please type r or c')                   
+if wavetype == 'r':
+    exp_file = os.path.join(save_path,'relaxed.pickle')
+elif wavetype == 'c':
+    exp_file = os.path.join(save_path,'concentrated.pickle')
+
+"""
+Continue to collect brain wave data until user specifies stop
+"""
+end_program = 'n'
+while end_program != 'y': #Loops every time user records data
+
+    input('Press <Enter> to start %.1f s data acquisition...' % ACQTIME)
+    print()
+    adc.startContinuousDifferentialConversion(2, 3, pga=VRANGE, sps=SPS)
+    time_series = np.zeros(nsamples, 'float')
+
+    t0 = time.perf_counter()
+    for i in range(nsamples): #Collects data every sinterval
+        st = time.perf_counter()
+        time_series[i] = 0.001*adc.getLastConversionResults() #Times 0.001 since adc measures in mV
+        time_series[i] -= 3.3 #ADC ground is 3.3 volts above circuit ground
+        while (time.perf_counter() - st) <= sinterval:
+            pass
+    t = time.perf_counter() - t0    
+    adc.stopContinuousConversion()
+    print('Time elapsed: %.9f s.' % t)
+    freq = np.fft.fftfreq(nsamples, d=1.0/SPS)
+    ps = get_power_spectrum(time_series)
+    rms = get_rms_voltage(ps, freq_min, freq_max, freq, nsamples)
+    print('RMS of Alpha Wave Voltage: ', rms)
+    
+    f1, ax1 = plt.subplots()
+    times = np.arange(0, ACQTIME, sinterval)
+    ax1.plot(times, time_series)
+    ax1.set(xlabel='Time (s)', ylabel='Voltage', title='Raw Signal')    
+    f1.show()
+    
+    f2, ax2 =plt.subplots()
+    plt.xlim(0,200)
+    ax2.plot(freq, ps)
+    ax2.set(xlabel='Frequency (Hz)', ylabel='Power Spectrum', title='Power Spectrum of FFT')    
+    f2.show()
+    
+    f3, ax3 =plt.subplots()
+    brain_wave = get_brain_wave(time_series,freq_min,freq_max,freq)
+    ax3.plot(times, time_series)
+    ax3.set(xlabel='Time (s)', ylabel='Voltage', title='Brain Alpha Wave')    
+    f3.show()
+    
+    while True:
+        save = input('Save this last run? (y/n)')
+        if save == 'y':
+            if os.path.exists(exp_file): #adds to existing file
+                file = open(exp_file, 'rb')
+                brain_data = pickle.load(file)
+                file.close()
+                brain_data.append(time_series)
+                file = open(exp_file, 'wb')
+                pickle.dump(brain_data, file)
+                file.close()
+            else: #creates new file to save
+                brain_data = [time_series]
+                file = open(exp_file, 'wb')
+                pickle.dump(brain_data, file)
+                file.close()
+            break            
+        elif save == 'n':
+            break
+        else:
+            print('Please type y or n.')
+    plt.close('all')
+    while True:
+        end_program = input('Do you want to end program? (y/n)')
+        if end_program in ['y', 'n']:
+            break
+        else:
+            print('Please enter \'y\' or \'n\' ')
+    print('\n')
+                     
+                     
+                     
+                     
+
