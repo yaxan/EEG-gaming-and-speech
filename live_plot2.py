fa5a228482c3e32e5bfbb1b8c27651284fb2bd8b
Saad Hossain <68381793+sdhossain@users.noreply.github.com>
2023-02-11 22:45:05 -0500

    Update live_plot2.py

--- live_plot2.py
+++ live_plot2.py
@@ -1,39 +1,19 @@
-"""
-Live plotting
-"""
-
-import os
-import sys
-sys.path.insert(1, os.path.dirname(os.getcwd())) #This allows importing files from parent folder
-import time
 import matplotlib.pyplot as plt
+from drawnow import drawnow
+import numpy as np
 
-plt.ion() ## Note this correction
-fig=plt.figure()
-plt.axis([0,10,-1,1])
+def make_fig():
+    plt.scatter(x, y)  # I think you meant this
 
-SPS = 860 #Samples per second to collect data. Options: 128, 250, 490, 920, 1600, 2400, 3300.
-VRANGE = 6144 #Full range scale in mV. Options: 256, 512, 1024, 2048, 4096, 6144.
-sinterval = 1.0/SPS
+plt.ion()  # enable interactivity
+fig = plt.figure()  # make a figure
 
-i = 0
-t=list()
-y=list()
+x = list()
+y = list()
 
-
-print("press q to exit program")
-
-while True: #Loops every time user records data
-
-    st = time.perf_counter()
-
-    chan = 0.45
-    y.append(chan)
-    t.append(time.perf_counter())
-
-    plt.plot(t, y, c = 'b')
-    plt.show()
-    plt.pause(0.1)
-
-    while (time.perf_counter() - st) <= sinterval:
-        pass
+for i in range(1000):
+    temp_y = np.random.random()
+    x.append(i)
+    y.append(temp_y)  # or any arbitrary update to your figure's data
+    i += 1
+    drawnow(make_fig)
