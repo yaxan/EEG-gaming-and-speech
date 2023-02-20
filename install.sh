cecb74f51bbd95b06ff9ca49b400981dc1e9d8f1
Saad Hossain <68381793+sdhossain@users.noreply.github.com>
2023-02-19 20:42:16 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,2 @@
+sudo apt-get update
+sudo apt-get install build-essential cmake pkg-config libjpeg-dev libtiff5-dev libjasper-dev libpng-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libfontconfig1-dev libcairo2-dev libgdk-pixbuf2.0-dev libpango1.0-dev libgtk2.0-dev libgtk-3-dev libatlas-base-dev gfortran libhdf5-dev libhdf5-serial-dev libhdf5-103 python3-pyqt5 python3-dev -y
