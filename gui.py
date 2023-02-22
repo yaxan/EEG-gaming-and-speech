9fe927c01c47794c4d6bc26a87b0b04944ec1e5d
yazan <ymasoud@uwaterloo.ca>
2023-02-22 00:18:41 -0500

    changed GUI to have 4 circles 8 10 12 14

--- gui.py
+++ gui.py
@@ -22,24 +22,27 @@ pygame.display.set_caption("Blinking Circles")
 font = pygame.font.Font(None, 36)
 
 # set the circle parameters
-circle_radius = 50
+circle_radius = 200
 circle_center_y = screen_height // 2
 circle_spacing = 700
 
 # set the circle frequencies
-frequency_1 = 7
+frequency_1 = 8
 frequency_2 = 10
-frequency_3 = 13
+frequency_3 = 12
+frequency_4 = 14
 
 # set the initial circle colors
 circle_color_1 = BLACK
 circle_color_2 = BLACK
 circle_color_3 = BLACK
+circle_color_4 = BLACK
 
 # set the initial times for the circles
 last_time_1 = pygame.time.get_ticks()
 last_time_2 = pygame.time.get_ticks()
 last_time_3 = pygame.time.get_ticks()
+last_time_4 = pygame.time.get_ticks()
 
 # run the game loop
 running = True
@@ -57,6 +60,7 @@ while running:
     time_diff_1 = current_time - last_time_1
     time_diff_2 = current_time - last_time_2
     time_diff_3 = current_time - last_time_3
+    time_diff_4 = current_time - last_time_4
 
     # check if the circles should change color
     if time_diff_1 >= 1000 / frequency_1:
@@ -70,32 +74,35 @@ while running:
     if time_diff_3 >= 1000 / frequency_3:
         circle_color_3 = WHITE if circle_color_3 == BLACK else BLACK
         last_time_3 = current_time
+    if time_diff_4 >= 1000 / frequency_3:
+        circle_color_4 = WHITE if circle_color_4 == BLACK else BLACK
+        last_time_4 = current_time
 
     # fill the background
     screen.fill(GREY)
 
     # draw the circles
-    circle_1_center_x = screen_width // 2 - circle_spacing
-    pygame.draw.circle(screen, circle_color_1, (circle_1_center_x, circle_center_y), circle_radius)
-
-    circle_2_center_x = screen_width // 2
-    pygame.draw.circle(screen, circle_color_2, (circle_2_center_x, circle_center_y), circle_radius)
-
-    circle_3_center_x = screen_width // 2 + circle_spacing
-    pygame.draw.circle(screen, circle_color_3, (circle_3_center_x, circle_center_y), circle_radius)
-
+    pygame.draw.circle(screen, circle_color_1, (screen_width//4, screen_height//4), circle_radius)
+    pygame.draw.circle(screen, circle_color_2, (3*screen_width//4, screen_height//4), circle_radius)
+    pygame.draw.circle(screen, circle_color_3, (screen_width//4, 3*screen_height//4), circle_radius)
+    pygame.draw.circle(screen, circle_color_4, (3*screen_width//4, 3*screen_height//4), circle_radius)
+    
     # add text to the circles
-    text_surface_1 = font.render(f"{frequency_1} Hz", True, WHITE)
-    text_rect_1 = text_surface_1.get_rect(center=(circle_1_center_x, circle_center_y + circle_radius + 20))
+    text_surface_1 = font.render(f"{frequency_1} Hz", True, (255, 255, 255))
+    text_rect_1 = text_surface_1.get_rect(center=(screen_width//4, screen_height//4 + circle_radius + 20))
     screen.blit(text_surface_1, text_rect_1)
-
-    text_surface_2 = font.render(f"{frequency_2} Hz", True, WHITE)
-    text_rect_2 = text_surface_2.get_rect(center=(circle_2_center_x, circle_center_y + circle_radius + 20))
+    
+    text_surface_2 = font.render(f"{frequency_2} Hz", True, (255, 255, 255))
+    text_rect_2 = text_surface_2.get_rect(center=(3*screen_width//4, screen_height//4 + circle_radius + 20))
     screen.blit(text_surface_2, text_rect_2)
 
     text_surface_3 = font.render(f"{frequency_3} Hz", True, (255, 255, 255))
-    text_rect_3 = text_surface_3.get_rect(center=(circle_3_center_x, circle_center_y + circle_radius + 20))
+    text_rect_3 = text_surface_3.get_rect(center=(screen_width//4, 3*screen_height//4 + circle_radius + 20))
     screen.blit(text_surface_3, text_rect_3)
+
+    text_surface_4 = font.render(f"{frequency_4} Hz", True, (255, 255, 255))
+    text_rect_4 = text_surface_4.get_rect(center=(3*screen_width//4, 3*screen_height//4 + circle_radius + 20))
+    screen.blit(text_surface_4, text_rect_4)
 
     # Update the screen
     pygame.display.update()
