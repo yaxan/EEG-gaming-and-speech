6ff5bdad04f4d6b435571ef994b4b7886ed551fd
Saad Hossain <saadhossain@Saads-MacBook-Air.local>
2023-02-23 02:35:02 -0500

    pseudo-coding general approach to SSVEP

--- SSVEP/SSVEP.py
+++ SSVEP/SSVEP.py
@@ -35,7 +35,7 @@ adc.data_rate = SPS
 raw_signal = np.zeros(nsamples)
 chan = AnalogIn(adc, ADS.P2, ADS.P3)
 
-fr1, fr2, fr3, fr4 = 8, 10, 12, 14 
+FR1, FR2, FR3, FR4 = 8, 10, 12, 14 
 
 def data():
 	t0 = time.perf_counter()
@@ -47,10 +47,10 @@ def data():
 			pass
 	t = time.perf_counter() - t0
 
-	ps1, rms1 = rms_voltage_power_spectrum(raw_signal, fr1, fr1, SPS, nsamples)
-	ps2, rms2 = rms_voltage_power_spectrum(raw_signal, fr2, fr2, SPS, nsamples)
-	ps3, rms3 = rms_voltage_power_spectrum(raw_signal, fr3, fr3, SPS, nsamples)
-	ps4, rms4 = rms_voltage_power_spectrum(raw_signal, fr4, fr4, SPS, nsamples)
+	ps1, rms1 = rms_voltage_power_spectrum(raw_signal, FR1, FR1, SPS, nsamples)
+	ps2, rms2 = rms_voltage_power_spectrum(raw_signal, FR2, FR2, SPS, nsamples)
+	ps3, rms3 = rms_voltage_power_spectrum(raw_signal, FR3, FR3, SPS, nsamples)
+	ps4, rms4 = rms_voltage_power_spectrum(raw_signal, FR4, FR4, SPS, nsamples)
 	
 	print("rms 1: ", rms1)
 	print("rms 2: ", rms2)
@@ -62,24 +62,55 @@ def data():
 	largest = max(rms1,rms2,rms3,rms4)
 
 	if (largest == rms1):
-		print("You're looking at: ", fr1, "Hz")
+		print("You're looking at: ", FR1, "Hz")
+		return 1
 	elif (largest == rms2):
-		print("You're looking at: ", fr2, "Hz")
+		print("You're looking at: ", FR2, "Hz")
+		return 2
 	elif (largest == rms3):
-		print("You're looking at: ", fr3, "Hz")
+		print("You're looking at: ", FR3, "Hz")
+		return 3
 	elif (largest == rms4):
-		print("You're looking at: ", fr4, "Hz")
-		
+		print("You're looking at: ", FR4, "Hz")
+		return 4
+	else:
+		print("What?")
 
-	
+	return 0
 	
 if __name__ == "__main__":
-	
-	process1 = multiprocessing.Process(target=blinking_circles, args=("a", "b", "c", "d", fr1, fr2, fr3, fr4))
-	
-	process1.start()
-	
-	data()
+
+	while True:
+		
+		# speech = get_text_from_speech
+		# get_prompts function to get 4 prompts 
+		# prompt_1, .. prompt_4 = get_prompts(speech)
+
+		prompt_1 = "BLA"
+		prompt_2 = "BLA"
+		prompt_3 = "BLA"
+		prompt_4 = "BLA"
+		
+		process1 = multiprocessing.Process(
+				target=blinking_circles, 
+				args=(
+					prompt_1, prompt_2, 
+					prompt_3, prompt_4, 
+					FR1, FR2, FR3, FR4
+				)
+			)
+		
+		process1.start() # GUI Appears on screen with prompts
+		
+		choice = data() # Data Collection Starts
+
+		# get chosen response prompt
+		# text to speech
+
+		# handle edge cases.
+
+
+
 
 	
 
