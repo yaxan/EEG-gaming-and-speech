07596cfab11ec8bd4baaee875c7686606571f794
Jaeyoung Kang <74623611+j97kang@users.noreply.github.com>
2023-02-24 17:39:55 -0500

    modified openai application

--- SSVEP/SSVEP.py
+++ SSVEP/SSVEP.py
@@ -1,8 +1,7 @@
 
 import sys
-sys.path.insert(1, os.path.dirname(os.getcwd())) #Allows importing files from parent folder
-
 import os
+sys.path.insert(1, os.path.dirname(os.getcwd())) #Allows importing files from parent folder
 import time
 import pickle
 import threading
@@ -13,108 +12,20 @@ from scipy import signal
 from scipy.signal import butter, sosfilt
 import matplotlib.pyplot as plt
 
-import board
-import busio
-import adafruit_ads1x15.ads1115 as ADS
-from adafruit_ads1x15.analog_in import AnalogIn
-
-import pyttsx3
-import speech_recognition as sr
-from gui import blinking_circles
-from analysis_data import rms_voltage_power_spectrum, brain_signal_extraction
-from SSVEP.speech import speech_to_text, text_to_speech
-from SSVEP.openai_application import get_prompts
+from openai_application import get_prompts
 
 
-#ADC Params
-ACQTIME = 5
-SPS = 860 #samples per second
-nsamples = int(ACQTIME*SPS)
-sinterval = 1.0/SPS
-
-#ADC Setup
-i2c = busio.I2C(board.SCL, board.SDA, frequency=1000000)
-adc = ADS.ADS1115(i2c)
-adc.mode = ADS.Mode.CONTINUOUS
-adc.gain = 1
-adc.data_rate = SPS
-raw_signal = np.zeros(nsamples)
-chan = AnalogIn(adc, ADS.P2, ADS.P3)
-
-FR1, FR2, FR3, FR4 = 8, 10, 12, 14 
-
-def data():
-	t0 = time.perf_counter()
-	for i in range(nsamples): #Collects data every interval
-		st = time.perf_counter()
-		raw_signal[i] = chan.value*(4.096/32767)
-		raw_signal[i] -= 3.3 #ADC ground is 3.3 volts above circuit ground
-		while (time.perf_counter() - st) <= sinterval:
-			pass
-	t = time.perf_counter() - t0
-
-	ps1, rms1 = rms_voltage_power_spectrum(raw_signal, FR1, FR1, SPS, nsamples)
-	ps2, rms2 = rms_voltage_power_spectrum(raw_signal, FR2, FR2, SPS, nsamples)
-	ps3, rms3 = rms_voltage_power_spectrum(raw_signal, FR3, FR3, SPS, nsamples)
-	ps4, rms4 = rms_voltage_power_spectrum(raw_signal, FR4, FR4, SPS, nsamples)
-	
-	print("rms 1: ", rms1)
-	print("rms 2: ", rms2)
-	print("rms 3: ", rms3)
-	print("rms 4: ", rms4)
-	
-	rms1, rms2, rms3, rms4;
-	
-	largest = max(rms1,rms2,rms3,rms4)
-
-	if (largest == rms1):
-		print("You're looking at: ", FR1, "Hz")
-		return 1
-	elif (largest == rms2):
-		print("You're looking at: ", FR2, "Hz")
-		return 2
-	elif (largest == rms3):
-		print("You're looking at: ", FR3, "Hz")
-		return 3
-	elif (largest == rms4):
-		print("You're looking at: ", FR4, "Hz")
-		return 4
-	else:
-		print("What?")
-
-	return 0
-	
 if __name__ == "__main__":
 
 	while True:
-		engine = pyttsx3.init()
-		engine.setProperty('rate', 150)
-		engine.setProperty('volume', 0.7)
-
-		recognizer = sr.Recognizer()
-		speech = speech_to_text(recognizer)
 		
+		speech = 'How are you?'
 		# get_prompts function to get 4 prompts 
 		prompt_1, prompt_2, prompt_3, prompt_4 = get_prompts(speech)
 		
-		process1 = multiprocessing.Process(
-				target=blinking_circles, 
-				args=(
-					prompt_1, prompt_2, 
-					prompt_3, prompt_4, 
-					FR1, FR2, FR3, FR4
-				)
-			)
-		
-		process1.start() # GUI Appears on screen with prompts
-		
-		choice = data() # Data Collection Starts
-
-		# get chosen response prompt
-		# text to speech
-
-		# handle edge cases.
-
+		print("1. " + prompt_1)
+		print("2. " + prompt_2)
+		print("3. " + prompt_3)
 
 
 
--- SSVEP/openai_application.py
+++ SSVEP/openai_application.py
@@ -1,208 +1,53 @@
 import openai
-import numpy as np
-#from adafruit_ads1x15.ads1x15 import ADS1x15
-#import adafruit_ads1x15.ads1115 as ADS
-#from adafruit_ads1x15.ads1x15 import Mode
-#from adafruit_ads1x15.analog_in import AnalogIn
-#import board
-#import busio
-import scipy as sp
 import time
-from speech.py import speech_to_text, text_to_speech
-import speech_recognition as sr
-import pyttsx3
 
 def get_prompts(text):
-    response = openai.Completion.create(
+    # Generate three prompts using GPT-3
+    message1 = openai.Completion.create(
         engine="davinci",
-        prompt=prompt,
-        max_tokens=50,
-        n=2,
-        temperature=0.5,
-        frequency_penalty=0.7,
-        presence_penalty=0.7,
-        stop='. '
-    )
-    time.sleep(1) 
-    message1 = response.choices[0].text.strip()
-    if ":" in message1:
-        message1 = message1.split(":")[1].strip()
-    if '\n' in message1:
-        message1 = message1.split("\n", 1)[0].strip()
-    if 'AI' in message1:
-        message1 = message1.split("AI", 1)[0].strip()
-    message2 = response.choices[1].text.strip()
-    if ":" in message2:
-        message2 = message2.split(":")[1].strip()
-    if '\n' in message2:
-        message2 = message2.split("\n", 1)[0].strip()
-    if 'AI' in message2:
-        message2 = message2.split("AI", 1)[0].strip()
-    return message1, message2, "More Options", "Bye"
-
-
-
-# Set OpenAI API key
-openai.api_key = "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"
-
-# Set the initial prompt
-ice_breaker = "Hello, how are you doing today?"
-prompt = f'AI: {ice_breaker}\n'
-
-#generate responses using OpenAI Text Completion API
-def generate_user_response(prompt):
-    '''
-    temperature: controls randomness of the generated responses
-    max_tokens: limits length of response
-    n = possible responses
-    frequency_penalty: higher decreases the likelihood of generating the same words multiple times -> controls diversity
-    presence_penalty: higher presence penalty decreases the likelihood of generating the same phreases across multiple responses -> diversity
-    stop: tells the code to stop printing at a value given (makes sure output is a one line output with one response)
-    '''
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt=prompt,
-        max_tokens=50,
-        n=2,
-        temperature=0.8,
-        frequency_penalty=0.7,
-        presence_penalty=0.3,
-        stop='. '
-    )
-    time.sleep(1) 
-    message1 = response.choices[0].text.strip()
-    if ":" in message1:
-        message1 = message1.split(":")[1].strip()
-    if '\n' in message1:
-        message1 = message1.split("\n", 1)[0].strip()
-    if 'AI' in message1:
-        message1 = message1.split("AI", 1)[0].strip()
-    message2 = response.choices[1].text.strip()
-    if ":" in message2:
-        message2 = message2.split(":")[1].strip()
-    if '\n' in message2:
-        message2 = message2.split("\n", 1)[0].strip()
-    if 'AI' in message2:
-        message2 = message2.split("AI", 1)[0].strip()
-    
-    return message1, message2
-
-def generate_AI_response(prompt):
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt=prompt,
+        prompt=f"Complete the following sentence: '{text}'\nAI response:",
         max_tokens=50,
         n=1,
+        stop=None,
         temperature=0.5,
-        frequency_penalty=0.7,
-        presence_penalty=0.7,
-        stop='.'
-    )
-    time.sleep(1) 
-    return response.choices[0].text.strip()
+    ).choices[0].text.strip()
 
+    message2 = openai.Completion.create(
+        engine="davinci",
+        prompt=f"What would a Human say in response to '{text}'?\nResponse:",
+        max_tokens=50,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    ).choices[0].text.strip()
 
-def generate_conversation_toAI(prompt):
-    #generate conversation to AI
-    message1, message2 = generate_user_response(prompt)
-        
-    print("1. " + message1)
-    print("2. " + message2)
-    print("3. Give me more options")
-    print("4. Quit")
-    user_choice = input("> ")
-        
-    while user_choice == '3':
-        #generate responses
-        message1, message2 = generate_user_response(prompt)
-        print("Bot: Here are two more options:")
-        print("1. " + message1)
-        print("2. " + message2)
-        print("3. Give me more options")
-        print("4. Quit")
-        user_choice = input("You: ")
-    
-    if user_choice == '1':
-        print("You: " + message1)
-        return user_choice, message1
-    elif user_choice == '2':
-        print("You: " + message2)
-        return user_choice, message2
-    elif user_choice == '4':
-        return user_choice, ""
+    message3 = openai.Completion.create(
+        engine="davinci",
+        prompt=f"Imagine a Human conversation about '{text}'. What would the Human respond?\nResponse:",
+        max_tokens=50,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    ).choices[0].text.strip()
+    if ":" in message1:
+        message1 = message1.split(":")[1].strip()
+    if '\n' in message1:
+        message1 = message1.split("\n", 1)[0].strip()
+    if 'AI' in message1:
+        message1 = message1.split("AI", 1)[0].strip()
+    if ":" in message2:
+        message2 = message2.split(":")[1].strip()
+    if '\n' in message2:
+        message2 = message2.split("\n", 1)[0].strip()
+    if 'AI' in message2:
+        message2 = message2.split("AI", 1)[0].strip()
+    if ":" in message3:
+        message3 = message3.split(":")[1].strip()
+    if '\n' in message3:
+        message3 = message3.split("\n", 1)[0].strip()
+    if 'AI' in message3:
+        message3 = message3.split("AI", 1)[0].strip()
+    return message1, message2, message3, "More Options"
 
-engine = pyttsx3.init()
-engine.setProperty('rate', 150)
-engine.setProperty('volume', 0.7)
-
-recognizer = sr.Recognizer()
-prompt = speech_to_text(recognizer)
-        
-while True:
-    print(prompt)
-    
-    #generate responses
-    message1, message2 = generate_user_response(prompt)
-    
-    print("1. " + message1)
-    print("2. " + message2)
-    print("3. Give me more options")
-    print("4. Quit")
-    
-    #CHANGE THIS FOR USER INPUT USING FREQUENCY
-    '''
-    pesudo code: 
-    user_choice = input using frequency
-    input = list of 4 frequency with certain percentage value
-    highest = input[whatever is wanted at what frequency]
-    user_choice = highest
-    '''
-    
-    user_choice = input("> ")
-
-    # generate two more responses if option 3 is chosen
-    while user_choice == '3':
-        #generate responses
-        message1, message2 = generate_user_response(prompt)
-        print("Bot: Here are two more options:")
-        print("1. " + message1)
-        print("2. " + message2)
-        print("3. Give me more options")
-        print("4. Quit")
-        user_choice = input("You: ")
-
-    # generate response based on user choice
-    if user_choice == "1":
-        print("You: " + message1)
-        text_to_speech(engine, message1)
-        print("what do you want to say now?")
-        #add user's choice as prompt to ask the ai
-        prompt += message1
-        #generate conversation to AI
-        user_choice, prompt = generate_conversation_toAI(prompt)
-        
-        if user_choice == "4":
-            print("Bot: Goodbye!")
-            break
-        else:
-            AI_response = speech_to_text(prompt) 
-            print("Bot: " + AI_response)
-            prompt = AI_response
-    elif user_choice == "2":
-        print("You: " + message2)
-        print("what do you want to say to the AI?")
-        #add user's choice as prompt to ask the ai
-        prompt += message2
-        #generate conversation to AI
-        user_choice, prompt = generate_conversation_toAI(prompt)
-        
-        if user_choice == "4":
-            print("Bot: Goodbye!")
-            break
-        else:
-            AI_response = generate_AI_response(prompt) 
-            print("Bot: " + AI_response)
-            prompt = AI_response
-    elif user_choice == "4":
-        print("Bot: Goodbye!")
-        break
+# Set OpenAI API key
+openai.api_key = "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"
